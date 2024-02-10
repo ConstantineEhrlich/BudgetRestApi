c92547d0b10bd39961bb7fff405c080e92c0890c
constantine <ferrumresistance@gmail.com>
2024-02-10 10:34:09 +0200

    Update Periodic Extensions to return IQueryable

--- BudgetModel/Extensions/IPeriodicExtensions.cs
+++ BudgetModel/Extensions/IPeriodicExtensions.cs
@@ -4,25 +4,25 @@ namespace BudgetModel.Extensions
 {
     public static class IPeriodicExtensions
     {
-        public static IEnumerable<T> UpToPeriod<T>(this IEnumerable<T> input, int toYear, int toPeriod) where T: IPeriodic
+        public static IQueryable<T> UpToPeriod<T>(this IQueryable<T> input, int toYear, int toPeriod) where T: IPeriodic
         {
             return input.Where(el => el.Year < toYear ||
                                      (el.Year == toYear && el.Period <= toPeriod));
         }
 
-        public static IEnumerable<T> ForPeriod<T>(this IEnumerable<T> input, int forYear, int forPeriod) where T : IPeriodic
+        public static IQueryable<T> ForPeriod<T>(this IQueryable<T> input, int forYear, int forPeriod) where T : IPeriodic
         {
             return input.Where(el => el.Year == forYear && el.Period == forPeriod);
         }
 
-        public static IEnumerable<T> StartingFrom<T>(this IEnumerable<T> input, int fromYear, int fromPeriod) where T : IPeriodic
+        public static IQueryable<T> StartingFrom<T>(this IQueryable<T> input, int fromYear, int fromPeriod) where T : IPeriodic
         {
             return input.Where(el => (el.Year == fromYear &&  el.Period >= fromPeriod)  ||
                                      (el.Year > fromYear));
         }
 
 
-        public static IEnumerable<T> Between<T>(this IEnumerable<T> input, int fromYear, int fromPeriod, int toYear, int toPeriod) where T : IPeriodic
+        public static IQueryable<T> Between<T>(this IQueryable<T> input, int fromYear, int fromPeriod, int toYear, int toPeriod) where T : IPeriodic
         {
             ValidateInput(fromYear, fromPeriod, toYear, toPeriod);
             return input.Where(el =>

d666ed31b24c7c4f67aa97e5ba7262e4c7554a7f
constantine <ferrumresistance@gmail.com>
2024-11-27 22:45:48 +0000

    supporting_reports (#5)
    Reviewed-on: https://git.home.arpa/mybudget.today/mybudget-api/pulls/5
    Co-authored-by: constantine <ferrumresistance@gmail.com>
    Co-committed-by: constantine <ferrumresistance@gmail.com>

--- BudgetWebApi/Controllers/ReportsController.cs
+++ BudgetWebApi/Controllers/ReportsController.cs
@@ -22,28 +22,57 @@ public class ReportsController : ControllerBase
     public IActionResult SummaryByCategory(string budgetId,
                                            [FromQuery, Range(1900, 2999)] int? year,
                                            [FromQuery, Range(1,12)] int? period)
-        => Ok(CreateReport<SummaryByPeriodAndCategory>(budgetId, year, period).Summarize());
+        => Ok(CreateReport<SummaryByPeriodAndCategory>(budgetId, year, period, ForOnePeriod).Summarize());
     
-
 
     [HttpGet]
     [Route("{budgetId}/[action]")]
     public IActionResult SummaryByOwner(string budgetId,
                                         [FromQuery, Range(1900, 2999)] int? year,
                                         [FromQuery, Range(1, 12)] int? period)
-        => Ok(CreateReport<SummaryByPeriodAndOwner>(budgetId, year, period).Summarize());
+        => Ok(CreateReport<SummaryByPeriodAndOwner>(budgetId, year, period, ForOnePeriod).Summarize());
+
+    [HttpGet]
+    [Route("{budgetId}/[action]")]
+    public IActionResult GetClosingBalance(string budgetId,
+                                           [FromQuery, Range(1900, 2999), Required] int year,
+                                           [FromQuery, Range(1, 12), Required] int period)
+        => Ok(CreateReport<BalanceToPeriodReport>(budgetId, year, period, UpToPeriod).Summarize());
 
 
+    [HttpGet]
+    [Route("{budgetId}/[action]")]
+    public IActionResult GetOpeningBalance(string budgetId,
+                                           [FromQuery, Range(1900, 2999), Required] int year,
+                                           [FromQuery, Range(1, 12), Required] int period)
+    {
+        // Calculate previous period
+        if (period == 1)
+            year -= 1;
+        else
+            period -= 1;
+        
+        return Ok(CreateReport<BalanceToPeriodReport>(budgetId, year, period, UpToPeriod).Summarize());    
+    }
+    
 
-    private IReportBuilder CreateReport<T>(string budget, int? year, int? period) where T: IReportBuilder, new()
+    private IReportBuilder CreateReport<T>(string budget,
+                                           int? year,
+                                           int? period,
+                                           Func<IReportBuilder, int, int, IReportBuilder> periodFunc)
+                                           where T: IReportBuilder, new()
     {
         _logger.LogInformation("Report {} requested for budget {}", typeof(T).Name, budget);
         string? requestingUser = User.Identity?.Name;
         IReportBuilder report = _reportFactory.CreateReport<T>(budget, requestingUser);
         if (year is not null && period is not null)
-            report = report.ForPeriod(year.Value, period.Value);
+            report = periodFunc(report, year.Value, period.Value);
         return report;
     }
+
+    private static IReportBuilder ForOnePeriod(IReportBuilder report, int year, int period)
+        => report.ForPeriod(year, period);
     
-    
+    private static IReportBuilder UpToPeriod(IReportBuilder report, int year, int period)
+        => report.ToPeriod(year, period);
 }
